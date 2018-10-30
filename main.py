3dd4b96d3f1b6c56b1ab8ac9c9c02d1f2a2015a3
johnpharmd <jhumphre@uw.edu>
2018-10-30 09:58:36 -0700

    Initial comt: changes to main.py, base.jinja2

--- main.py
+++ main.py
@@ -3,19 +3,37 @@ import base64
 
 from flask import Flask, render_template, request, redirect, url_for, session
 
-from model import Donation 
+from model import Donor, Donation 
 
 app = Flask(__name__)
+
 
 @app.route('/')
 def home():
     return redirect(url_for('all'))
+
 
 @app.route('/donations/')
 def all():
     donations = Donation.select()
     return render_template('donations.jinja2', donations=donations)
     
+
+@app.route('/create/', methods=['GET', 'POST'])
+def create():
+    if request.method == 'GET':
+        return render_template('create.jinja2')
+
+    if request.method == 'POST':
+        try:
+            donor = Donor.select().where(Donor.name == request.form['name']).get()
+            donation = Donation(value=int(request.form['value']), donor=donor)
+            donation.save()
+
+            return home()
+        except Donor.DoesNotExist:
+            return render_template('create.jinja2', error="Donor not found.")
+
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 6738))
